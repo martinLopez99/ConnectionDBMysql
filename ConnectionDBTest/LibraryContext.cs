df163f1d9e66280ac27e86ecd69292cedba90e93
Martin Lopez <tincho1597lopez@gmail.com>
2024-01-15 19:13:43 -0300

    Add project files.

---
+++ ConnectionDBTest/LibraryContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore.Extensions;
+using static ConnectionDBTest.LibraryModel;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace ConnectionDBTest
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+
+        public DbSet<Publisher> Publisher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=*****;database=****;user=****;password=******");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Publisher>(entity =>
+            {
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasKey(e => e.ISBN);
+                entity.Property(e => e.Title).IsRequired();
+                entity.HasOne(d => d.Publisher)
+                  .WithMany(p => p.Books);
+            });
+        }
+    }
+}
---
+++ ConnectionDBTest/LibraryModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionDBTest
+{
+    public class LibraryModel
+    {
+        public class Book
+        {
+            public string ISBN { get; set; }
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string Language { get; set; }
+            public int Pages { get; set; }
+            public virtual Publisher Publisher { get; set; }
+        }
+
+        public class Publisher
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public virtual ICollection<Book> Books { get; set; }
+        }
+    }
+}
